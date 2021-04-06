e9befe230edcb9ae8ade652cceaa2c90511e6988
jawaria Bashir <62**323@gmail.com>
2021-04-06 19:43:40 +0100

    ludo_task
    data update in project

--- ludo_task/lib/main.dart
+++ ludo_task/lib/main.dart
@@ -143,7 +143,79 @@ class _DicePageState extends State<DicePage>
     },
     child: Image.asset('images/dice$Bleft.png'),
     ),
+    ),Expanded(
+    child: TextButton(
+    onPressed: () {
+    setState(() {
+    if (dice2count < 10) {
+    Bright = Random().nextInt(6) + 1;
+    dice2 = dice2 + Bright;
+    dice2count = dice2count + 1;
+    total = total + Bright;
+    }
+    ;
+    if (dice1 > dice2 &&
+    dice1 > dice3 &&
+    dice1 > dice4) {
+    winner = dice1;
+    winn = 1;
+    } else if (dice2 > dice1 &&
+    dice2 > dice3 &&
+    dice2 > dice4) {
+    winner = dice2;
+    winn = 2;
+    } else if (dice3 > dice1 &&
+    dice3 > dice2 &&
+    dice3 > dice4) {
+    winner = dice3;
+    winn = 3;
+    } else {
+    winner = dice4;
+    winn = 4;
+    }
+    ;
+    if (dice1count >= dice2count &&
+    dice3count >= dice2count &&
+    dice4count >= dice2count) {
+    if (limit > 0) {
+    limit = limit - 1;
+    }
+    }
+    ;
+    if (dice1count == 10 &&
+    dice2count == 10 &&
+    dice3count == 10 &&
+    dice4count == 10) {
+    AlertDialog alert = AlertDialog(
+    title: Text('Winner: Dice $winn'),
+    content: Text('Points: $winner points'),
+    actions: [
+    FlatButton(
+    onPressed: () {
+    Navigator.of(context).pop();
+    },
+    child: Text('Ok'),
     ),
+    FlatButton(
+    onPressed: () {
+    setState(() {
+    total = 0;
+    winner = 0;
+    winn = 0;
+    dice1 = 0;
+    dice2 = 0;
+    dice3 = 0;
+    dice4 = 0;
+    limit = 10;
+    left = 1;
+    right = 1;
+    Bleft = 1;
+    Bright = 1;
+    dice1count = 0;
+    dice2count = 0;
+    dice3count = 0;
+    dice4count = 0;
+    });
 
 
 
