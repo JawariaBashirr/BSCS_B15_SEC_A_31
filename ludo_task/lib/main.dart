86151232c662b9c2019040a84e676f5476cf3604
jawaria Bashir <62**323@gmail.com>
2021-04-06 19:12:29 +0100

    ludo_task
    update data in project

--- ludo_task/lib/main.dart
+++ ludo_task/lib/main.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 void main() {
-
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Dicee'),
+          backgroundColor: Colors.grey,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+class _DicePageState extends State<DicePage>
 }
 
 
